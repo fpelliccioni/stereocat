ec6286bad3313e29700d645c4e88b3d178593c34
Fernando Pelliccioni <fpelliccioni@gmail.com>
2012-08-23 15:54:53 -0300

    refactoring

--- cpp_test/main.cpp
+++ cpp_test/main.cpp
@@ -1,7 +1,11 @@
 // main.cpp
 
+#include <iostream>
+#include <cstdio>
+#include <thread>
+
 #include <windows.h>
-#include <stdio.h>
+
 #include <mmsystem.h>
 #include <mmdeviceapi.h>
 
@@ -15,7 +19,7 @@ int do_everything(int argc, LPCWSTR argv[])
 	HRESULT hr = S_OK;
 
 	// parse command line
-	CPrefs prefs(argc, argv, hr);
+	CPrefs prefs(argc, argv, hr);	//TODO: remove
 	if (FAILED(hr)) 
 	{
 		printf("CPrefs::CPrefs constructor failed: hr = 0x%08x\n", hr);
@@ -45,134 +49,163 @@ int do_everything(int argc, LPCWSTR argv[])
 		return -__LINE__;
 	}
 
-	// create arguments for loopback capture thread
-	LoopbackCaptureThreadFunctionArguments threadArgs;
-	threadArgs.hr = E_UNEXPECTED; // thread will overwrite this
-	threadArgs.pMMDevice = prefs.m_pMMDevice;
-	threadArgs.bInt16 = prefs.m_bInt16;
-	threadArgs.hFile = prefs.m_hFile;
-	threadArgs.hStartedEvent = hStartedEvent;
-	threadArgs.hStopEvent = hStopEvent;
-	threadArgs.nFrames = 0;
+	//// create arguments for loopback capture thread
+	//LoopbackCaptureThreadFunctionArguments threadArgs;
+	//threadArgs.hr = E_UNEXPECTED; // thread will overwrite this
+	//threadArgs.pMMDevice = prefs.m_pMMDevice;
+	//threadArgs.bInt16 = prefs.m_bInt16;
+	//threadArgs.hFile = prefs.m_hFile;
+	//threadArgs.hStartedEvent = hStartedEvent;
+	//threadArgs.hStopEvent = hStopEvent;
+	//threadArgs.nFrames = 0;
 
-	HANDLE hThread = CreateThread(
-		NULL, 0,
-		LoopbackCaptureThreadFunction, &threadArgs,
-		0, NULL
-		);
+	//HANDLE hThread = CreateThread(
+	//	NULL, 0,
+	//	LoopbackCaptureThreadFunction, &threadArgs,
+	//	0, NULL
+	//	);
 
-	if (NULL == hThread) 
+	//if (NULL == hThread) 
+	//{
+	//	printf("CreateThread failed: last error is %u\n", GetLastError());
+	//	CloseHandle(hStopEvent);
+	//	CloseHandle(hStartedEvent);
+	//	return -__LINE__;
+	//}
+
+	HANDLE hThread;
+
+
+	LoopbackCaptureClass lcc;
+	lcc.hr = E_UNEXPECTED; // thread will overwrite this
+	lcc.pMMDevice = prefs.m_pMMDevice;
+	lcc.bInt16 = prefs.m_bInt16;
+	lcc.hFile = prefs.m_hFile;
+	lcc.hStartedEvent = hStartedEvent;
+	lcc.hStopEvent = hStopEvent;
+	lcc.nFrames = 0;
+	
+	std::thread work_thread(lcc);
+
+
+	while(true)
 	{
-		printf("CreateThread failed: last error is %u\n", GetLastError());
-		CloseHandle(hStopEvent);
-		CloseHandle(hStartedEvent);
-		return -__LINE__;
+		std::cout << ".";
+		std::this_thread::sleep_for( std::chrono::milliseconds(1000) );
 	}
 
-	// wait for either capture to start or the thread to end
-	HANDLE waitArray[2] = { hStartedEvent, hThread };
-	DWORD dwWaitResult;
-	dwWaitResult = WaitForMultipleObjects(
-		ARRAYSIZE(waitArray), waitArray,
-		FALSE, INFINITE
-		);
 
-	if (WAIT_OBJECT_0 + 1 == dwWaitResult) 
-	{
-		printf("Thread aborted before starting to loopback capture: hr = 0x%08x\n", threadArgs.hr);
-		CloseHandle(hStartedEvent);
-		CloseHandle(hThread);
-		CloseHandle(hStopEvent);
-		return -__LINE__;
-	}
+	work_thread.join();
 
-	if (WAIT_OBJECT_0 != dwWaitResult) 
-	{
-		printf("Unexpected WaitForMultipleObjects return value %u", dwWaitResult);
-		CloseHandle(hStartedEvent);
-		CloseHandle(hThread);
-		CloseHandle(hStopEvent);
-		return -__LINE__;
-	}
 
-	CloseHandle(hStartedEvent);
 
-	HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+	
 
-	if (INVALID_HANDLE_VALUE == hStdIn) 
-	{
-		printf("GetStdHandle returned INVALID_HANDLE_VALUE: last error is %u\n", GetLastError());
-		SetEvent(hStopEvent);
-		WaitForSingleObject(hThread, INFINITE);
-		CloseHandle(hStartedEvent);
-		CloseHandle(hThread);
-		CloseHandle(hStopEvent);
-		return -__LINE__;
-	}
 
-	printf("Press Enter to quit...\n");
+	//// wait for either capture to start or the thread to end
+	//HANDLE waitArray[2] = { hStartedEvent, hThread };
+	//DWORD dwWaitResult;
+	//dwWaitResult = WaitForMultipleObjects(
+	//	ARRAYSIZE(waitArray), waitArray,
+	//	FALSE, INFINITE
+	//	);
 
-	// wait for the thread to terminate early
-	// or for the user to press (and release) Enter
-	HANDLE rhHandles[2] = { hThread, hStdIn };
+	//if (WAIT_OBJECT_0 + 1 == dwWaitResult) 
+	//{
+	//	printf("Thread aborted before starting to loopback capture: hr = 0x%08x\n", threadArgs.hr);
+	//	CloseHandle(hStartedEvent);
+	//	CloseHandle(hThread);
+	//	CloseHandle(hStopEvent);
+	//	return -__LINE__;
+	//}
 
-	bool bKeepWaiting = true;
-	while (bKeepWaiting) 
-	{
+	//if (WAIT_OBJECT_0 != dwWaitResult) 
+	//{
+	//	printf("Unexpected WaitForMultipleObjects return value %u", dwWaitResult);
+	//	CloseHandle(hStartedEvent);
+	//	CloseHandle(hThread);
+	//	CloseHandle(hStopEvent);
+	//	return -__LINE__;
+	//}
 
-		dwWaitResult = WaitForMultipleObjects(2, rhHandles, FALSE, INFINITE);
+	//CloseHandle(hStartedEvent);
 
-		switch (dwWaitResult) 
-		{
+	//HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
 
-		case WAIT_OBJECT_0: // hThread
-			printf("The thread terminated early - something bad happened\n");
-			bKeepWaiting = false;
-			break;
+	//if (INVALID_HANDLE_VALUE == hStdIn) 
+	//{
+	//	printf("GetStdHandle returned INVALID_HANDLE_VALUE: last error is %u\n", GetLastError());
+	//	SetEvent(hStopEvent);
+	//	WaitForSingleObject(hThread, INFINITE);
+	//	CloseHandle(hStartedEvent);
+	//	CloseHandle(hThread);
+	//	CloseHandle(hStopEvent);
+	//	return -__LINE__;
+	//}
 
-		case WAIT_OBJECT_0 + 1: // hStdIn
-			// see if any of them was an Enter key-up event
-			INPUT_RECORD rInput[128];
-			DWORD nEvents;
-			if (!ReadConsoleInput(hStdIn, rInput, ARRAYSIZE(rInput), &nEvents)) 
-			{
-				printf("ReadConsoleInput failed: last error is %u\n", GetLastError());
-				SetEvent(hStopEvent);
-				WaitForSingleObject(hThread, INFINITE);
-				bKeepWaiting = false;
-			} 
-			else 
-			{
-				for (DWORD i = 0; i < nEvents; i++) 
-				{
-					if (
-						KEY_EVENT == rInput[i].EventType &&
-						VK_RETURN == rInput[i].Event.KeyEvent.wVirtualKeyCode &&
-						!rInput[i].Event.KeyEvent.bKeyDown
-						) 
-					{
-						printf("Stopping capture...\n");
-						SetEvent(hStopEvent);
-						WaitForSingleObject(hThread, INFINITE);
-						bKeepWaiting = false;
-						break;
-					}
-				}
-				// if none of them were Enter key-up events,
-				// continue waiting
-			}
-			break;
+	//printf("Press Enter to quit...\n");
 
-		default:
-			printf("WaitForMultipleObjects returned unexpected value 0x%08x\n", dwWaitResult);
-			SetEvent(hStopEvent);
-			WaitForSingleObject(hThread, INFINITE);
-			bKeepWaiting = false;
-			break;
-		}
-	}
+	//// wait for the thread to terminate early
+	//// or for the user to press (and release) Enter
+	//HANDLE rhHandles[2] = { hThread, hStdIn };
 
-	DWORD exitCode;
+	//bool bKeepWaiting = true;
+	//while (bKeepWaiting) 
+	//{
+
+	//	dwWaitResult = WaitForMultipleObjects(2, rhHandles, FALSE, INFINITE);
+
+	//	switch (dwWaitResult) 
+	//	{
+
+	//	case WAIT_OBJECT_0: // hThread
+	//		printf("The thread terminated early - something bad happened\n");
+	//		bKeepWaiting = false;
+	//		break;
+
+	//	case WAIT_OBJECT_0 + 1: // hStdIn
+	//		// see if any of them was an Enter key-up event
+	//		INPUT_RECORD rInput[128];
+	//		DWORD nEvents;
+	//		if (!ReadConsoleInput(hStdIn, rInput, ARRAYSIZE(rInput), &nEvents)) 
+	//		{
+	//			printf("ReadConsoleInput failed: last error is %u\n", GetLastError());
+	//			SetEvent(hStopEvent);
+	//			WaitForSingleObject(hThread, INFINITE);
+	//			bKeepWaiting = false;
+	//		} 
+	//		else 
+	//		{
+	//			for (DWORD i = 0; i < nEvents; i++) 
+	//			{
+	//				if (
+	//					KEY_EVENT == rInput[i].EventType &&
+	//					VK_RETURN == rInput[i].Event.KeyEvent.wVirtualKeyCode &&
+	//					!rInput[i].Event.KeyEvent.bKeyDown
+	//					) 
+	//				{
+	//					printf("Stopping capture...\n");
+	//					SetEvent(hStopEvent);
+	//					WaitForSingleObject(hThread, INFINITE);
+	//					bKeepWaiting = false;
+	//					break;
+	//				}
+	//			}
+	//			// if none of them were Enter key-up events,
+	//			// continue waiting
+	//		}
+	//		break;
+
+	//	default:
+	//		printf("WaitForMultipleObjects returned unexpected value 0x%08x\n", dwWaitResult);
+	//		SetEvent(hStopEvent);
+	//		WaitForSingleObject(hThread, INFINITE);
+	//		bKeepWaiting = false;
+	//		break;
+	//	}
+	//}
+
+/*	DWORD exitCode;
 	if (!GetExitCodeThread(hThread, &exitCode))
 	{
 		printf("GetExitCodeThread failed: last error is %u\n", GetLastError());
@@ -198,7 +231,7 @@ int do_everything(int argc, LPCWSTR argv[])
 	}
 
 	CloseHandle(hThread);
-	CloseHandle(hStopEvent);
+	CloseHandle(hStopEvent)*/;
 
 	// everything went well... fixup the fact chunk in the file
 	MMRESULT result = mmioClose(prefs.m_hFile, 0);
@@ -239,14 +272,24 @@ int do_everything(int argc, LPCWSTR argv[])
 	}
 
 	// write the correct data to the fact chunk
+	//LONG lBytesWritten = mmioWrite(
+	//	prefs.m_hFile,
+	//	reinterpret_cast<PCHAR>(&threadArgs.nFrames),
+	//	sizeof(threadArgs.nFrames)
+	//	);
+	//if (lBytesWritten != sizeof(threadArgs.nFrames)) 
+	//{
+	//	printf("Updating the fact chunk wrote %u bytes; expected %u\n", lBytesWritten, (UINT32)sizeof(threadArgs.nFrames));
+	//	return -__LINE__;
+	//}
 	LONG lBytesWritten = mmioWrite(
 		prefs.m_hFile,
-		reinterpret_cast<PCHAR>(&threadArgs.nFrames),
-		sizeof(threadArgs.nFrames)
+		reinterpret_cast<PCHAR>(&lcc.nFrames),
+		sizeof(lcc.nFrames)
 		);
-	if (lBytesWritten != sizeof(threadArgs.nFrames)) 
+	if (lBytesWritten != sizeof(lcc.nFrames)) 
 	{
-		printf("Updating the fact chunk wrote %u bytes; expected %u\n", lBytesWritten, (UINT32)sizeof(threadArgs.nFrames));
+		printf("Updating the fact chunk wrote %u bytes; expected %u\n", lBytesWritten, (UINT32)sizeof(lcc.nFrames));
 		return -__LINE__;
 	}
 
